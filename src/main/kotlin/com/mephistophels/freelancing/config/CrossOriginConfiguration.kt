3fad74e90e746d8a11d51a767b2c657f9705aa0e
Angoga <Anogeorgy05@gmail.com>
2023-10-04 16:02:44 +0300

    try fix cors

---
+++ src/main/kotlin/com/mephistophels/freelancing/config/CrossOriginConfiguration.kt
@@ -0,0 +1,21 @@
+package com.mephistophels.freelancing.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+
+@Configuration
+class CrossOriginConfiguration {
+    @Bean
+    fun corsFilter(): CorsFilter {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration()
+        config.allowCredentials = true
+        config.addAllowedOrigin("*")
+        config.addAllowedHeader("*")
+        config.addAllowedMethod("*")
+        return CorsFilter(source)
+    }
+}
